01d02fe214df18a2b118c4ca8067f8920a91aa55
Mitch <Couturier>
2020-01-31 22:49:31 -0500

    added base module definition

---
+++ GameJam2020_RobotPlatformer/Assets/Scripts/ModuleActions/DoubleJump.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJump : BaseModule
+{
+    public override void apply(GameObject player)
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(Vector3.up * 5 * Time.deltaTime);
+        }
+    }
+}
---
+++ GameJam2020_RobotPlatformer/Assets/Scripts/PlayerModuleManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerModuleManager : MonoBehaviour
+{
+    // list of modules the player has obtained
+    List<BaseModule> obtainedModules;
+
+    void Start()
+    {
+        obtainedModules = new List<BaseModule>();
+        obtainedModules.Add(new DoubleJump());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obtainedModules[0].apply(gameObject);
+    }
+}
