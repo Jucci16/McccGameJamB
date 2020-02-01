e6b45f29cb58b27c7ebce8f21085329cffae80c5
Mitch <Couturier>
2020-02-01 15:35:04 -0500

    add player battery management + Battery Prefabs

---
+++ GameJam2020_RobotPlatformer/Assets/Scripts/Triggers/BatteryTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryTrigger : MonoBehaviour
+{
+    public GameObject textOverlay;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // add a battery to the user's inventory
+            PlayerBatteryManager playerBatteryManager = collision.gameObject.GetComponent<PlayerBatteryManager>();
+            playerBatteryManager.addBatteryToInventory();
+
+            // display the text overlay for the battery
+            GameObject overlayObject = Instantiate(textOverlay);
+            overlayObject.GetComponent<ModuleTextOverlay>().setText("Battery Obtained");
+
+            // now delete this game object from the screen
+            Destroy(this.gameObject);
+        }
+    }
+}
