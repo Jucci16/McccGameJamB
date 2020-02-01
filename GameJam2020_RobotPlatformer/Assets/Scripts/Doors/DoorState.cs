682dc61acc2aca6f4bac0fad6393e056f1716fcb
Mitch <Couturier>
2020-02-01 16:30:39 -0500

    Added Door and Door Switch

---
+++ GameJam2020_RobotPlatformer/Assets/Scripts/Doors/DoorState.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorState : MonoBehaviour
+{
+    /// <summary>
+    /// True if the door is open
+    /// </summary>
+    private bool isOpen = false;
+    /// <summary>
+    /// Initial position of the door
+    /// </summary>
+    private Vector3 initialPosition;
+    /// <summary>
+    /// Height to move the door
+    /// </summary>
+    private float heightToMove;
+
+    private float doorSpeed = 2f;
+
+    private void Start()
+    {
+        initialPosition = gameObject.transform.position;
+        heightToMove = gameObject.transform.localScale.y;
+    }
+
+    void Update()
+    {
+        // if door should be open/opening
+        if(isOpen)
+        {
+            // if door is not fully open, continue opening
+            if (gameObject.transform.position.y < (initialPosition.y + heightToMove))
+            {
+                gameObject.transform.Translate(new Vector3(0, Time.deltaTime * doorSpeed, 0));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Open the door
+    /// </summary>
+    public void openDoor()
+    {
+        isOpen = true;
+    }
+}
