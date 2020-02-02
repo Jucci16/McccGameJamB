35ddc5932cf66aae7134159691724a0bb8091a93
Mitchell John Couturier <couturmi@mail.gvsu.edu>
2020-02-02 13:31:30 -0500

    added help text at the start of the game

--- GameJam2020_RobotPlatformer/Assets/Scripts/ModuleTextOverlay.cs
+++ GameJam2020_RobotPlatformer/Assets/Scripts/ModuleTextOverlay.cs
@@ -9,10 +9,16 @@ public class ModuleTextOverlay : MonoBehaviour
     private float timeLeft;
     private bool isDisplaying = false;
 
+    private const string startingText1 = "Fix the robot by obtaining modules.";
+    private const string startingText2 = "Select modules by pressing [1,2,3,4,5]";
+
     void Start()
     {
-        // set display time to the default
-        timeLeft = defaultDisplayTime;
+        // set starting text color
+        Text textObject = GetComponent<Text>();
+        textObject.color = Color.blue;
+        // set starting text
+        setText(startingText1);
     }
 
 
@@ -25,8 +31,18 @@ public class ModuleTextOverlay : MonoBehaviour
             if (timeLeft <= 0)
             {
                 Text textObject = GetComponent<Text>();
-                textObject.text = "";
-                isDisplaying = false;
+                if (textObject.text.Equals(startingText1))
+                {
+                    setText(startingText2);
+                }
+                else
+                {
+                    // reset color
+                    textObject.color = Color.white;
+                    // reset text
+                    textObject.text = "";
+                    isDisplaying = false;
+                }
             }
         }
     }
